6c3c6b163f78ed06faeaac4ff1364034f9c75f15
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-11-21 19:37:51 +0200

    add doctrine + MoviesController

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    #[Route('/movie', name: 'app_movies')]
+    public function index(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MoviesController.php',
+        ]);
+    }
+    
+    /**
+     * oldMethod
+     *
+     * @Route("/old", name="old")
+     */
+    public function oldMethod(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Old Method.',
+            'path' => 'src/Controller/MoviesController.php',
+        ]);
+    }
+}
