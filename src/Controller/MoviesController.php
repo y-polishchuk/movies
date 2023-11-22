2c740a14c9ae8f8e6ff4b7ea705244a26bf97ee7
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-11-22 11:39:24 +0200

    route parameters

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -8,25 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movies')]
-    public function index(): JsonResponse
+    #[Route('/movies/{name}', name: 'app_movies', defaults: [ 'name' => null ], methods:['GET', 'HEAD'])]
+    public function index($name): JsonResponse
     {
         return $this->json([
-            'message' => 'Welcome to your new controller!',
+            'message' => $name,
             'path' => 'src/Controller/MoviesController.php',
         ]);
     }
-    
-    /**
-     * oldMethod
-     *
-     * @Route("/old", name="old")
-     */
-    public function oldMethod(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Old Method.',
-            'path' => 'src/Controller/MoviesController.php',
-        ]);
-    }
+
 }
