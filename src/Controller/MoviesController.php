427161c4e895fcaeac2b0534dffb60099d54a05d
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-11-23 17:19:22 +0200

    repository

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+    private $em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
-        $movies = ['Avengers: Endgame!', 'Inception', 'Loki', 'Black Widow'];
-        return $this->render('index.html.twig', array(
-            'movies' => $movies,
-        ));
-    }
+        // findAll() - SELECT * FROM movies;
+        // find() - SELECT * FROM movies WHERE id=5;
+        // findBy() - SELECT * FROM movies ORDER BY id DESC;
+        // findOneBy() - SELECT * FROM movies WHERE id = 6 AND title = 'The Dark Knight' ORDER BY id DESC;
+        // count() - SELECT COUNT() FROM movies WHERE id = 5;
 
+        $repository = $this->em->getRepository(Movie::class);
+        $movies = $repository->getClassName();
+
+        dd($movies);
+
+        return $this->render('index.html.twig');
+    }
 }
