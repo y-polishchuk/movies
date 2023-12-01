1fcfdf304412875410bb70a5502ca8a3a6561230
MandalorianFighter <yanush.polishchuk@gmail.com>
2023-12-01 16:07:35 +0200

    crud:delete blog items

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -108,6 +108,16 @@ class MoviesController extends AbstractController
         ]);
     }
 
+    #[Route('/movies/delete/{id}', methods: ['GET', 'DELETE'], name: 'delete_movie')]
+    public function delete($id)
+    {
+        $movie = $this->movieRepository->find($id);
+        $this->em->remove($movie);
+        $this->em->flush();
+
+        return $this->redirectToRoute('movies');
+    }
+
     #[Route('/movies/{id}', methods: ['GET'], name: 'show_movie')]
     public function show($id): Response
     {
